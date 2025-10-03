e35197b9245bf51e243dd94bc9a81e9f202c9e6b
ankit013002 <112412889+ankit013002@users.noreply.github.com>
2025-10-02 21:20:47 -0400

    Includes initial backend.

---
+++ recipe-blog-backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express backend!");
+});
+
+app.post("/api/data", (req, res) => {
+  const body = req.body;
+  res.json({ message: "Data received!", data: body });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
